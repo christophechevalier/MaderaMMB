1b498e9870b329da6ca6a4753acd0956668370b3
Alexandre-Schwarze <alexschwarze@live.fr>
2016-11-11 15:52:57 +0100

    - Modification du point d'entrée - Création du template

--- Test_envoi_msg/Test_envoi_msg/App.xaml.cs
+++ Test_envoi_msg/Test_envoi_msg/App.xaml.cs
@@ -5,26 +5,13 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using Madera_MMB.View_Crtl;
 
-namespace Madera_MMB
+namespace Test_envoi_msg
 {
     /// <summary>
     /// Logique d'interaction pour App.xaml
     /// </summary>
     public partial class App : Application
     {
-        [STAThread]
-
-        public static void Main()
-        {
-
-            App app = new App();
-
-            app.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
-
-            app.Run();
-
-        }
     }
 }
---
+++ Test_envoi_msg/Test_envoi_msg/DoubleTextBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Test_envoi_msg
+{
+    class DoubleTextBox : TextBox
+    {
+        static DoubleTextBox()
+        {
+        }
+
+        protected override void OnTextInput(TextCompositionEventArgs e)
+        {
+            e.Handled = !DoubleCharChecker(e.Text);
+            base.OnTextInput(e);
+        }
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            e.Handled = (e.Key == Key.Space);
+            base.OnPreviewKeyDown(e);
+        }
+
+        private bool DoubleCharChecker(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c.Equals('-'))
+                    return true;
+
+                else if (c.Equals('.'))
+                    return true;
+
+                else if (Char.IsNumber(c))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
