9d30b0ed11d8581aaf2fd17fcbdaaede91d796bb
christophechevalier <christophe.chevalier1@viacesi.fr>
2016-11-13 13:32:38 +0100

    Première partie de la création gestion client
    - Ajout du menu gestion client
    - Ajout liste des clients
    - Ajout sélection d'un client pour créer un nouveau projet
    - Ajout footer pour avec la description d'un projet$
    - Amélioration du template de base

---
+++ app/Madera MMB/View_Crtl/GestionClient.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Madera_MMB.View_Crtl
+{
+    /// <summary>
+    /// Logique d'interaction pour GestionClient.xaml
+    /// </summary>
+    public partial class GestionClient : Page
+    {
+        List<Panel> listPanel = new List<Panel>();
+        int index;
+
+        public GestionClient()
+        {
+            InitializeComponent();
+        }
+        private void Btn_Ouvrir_Projet_Client_Click(object sender, RoutedEventArgs e)
+        { 
+        
+        }
+        private void Btn_List_Client_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Btn_Creer_Client_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Btn_Creer_Projet_Click(object sender, RoutedEventArgs e)
+        {
+            ModalCreationNouveauProjet.Visibility = System.Windows.Visibility.Visible;
+        }
+        private void Btn_Ouvrir_Projet_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Btn_Se_Deconnecter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Btn_Valider_Click(object sender, RoutedEventArgs e)
+        {
+            ModalCreationNouveauProjet.Visibility = System.Windows.Visibility.Collapsed;
+        }
+        private void Btn_Retour_Click(object sender, RoutedEventArgs e)
+        {
+            ModalCreationNouveauProjet.Visibility = System.Windows.Visibility.Hidden;
+        }
+        private void Select_Nom_Client_Click(object sender, RoutedEventArgs e)
+        {
+            CbSelectNomClient.SelectedIndex = 0;
+        }
+    }
+}
