89fb998058bdd7edc9ff123b77d79594220b0b40
christophechevalier <christophe.chevalier1@viacesi.fr>
2016-11-20 17:19:41 +0100

    Suite création vue liste des clients
    - Ajout liste de client
    - Ajour détails d'un client sélectionner

--- app/Madera MMB/View_Crtl/GestionClient.xaml.cs
+++ app/Madera MMB/View_Crtl/GestionClient.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -25,13 +26,39 @@ namespace Madera_MMB.View_Crtl
         {
             InitializeComponent();
             Initialize_Labels();
+            Initialize_Liste_Clients_Wrapper();
         }
         #endregion
 
-        #region Initialisation Container
+        #region Initialisation Containers
         private void Initialize_Labels()
         {
-            nomCommercial.Content += "José Répas";
+            NomCommercial.Content += "José Répas";
+        }
+        private void Initialize_Liste_Clients_Wrapper()
+        {
+            List<String> nomsComplet = new List<string>();
+            List<String> noms = new List<string>();
+            List<String> prenoms = new List<string>();
+            List<String> telephones = new List<string>();
+            List<String> emails = new List<string>();
+            List<String> adresses = new List<string>();
+            List<String> codepostal = new List<string>();
+            List<String> villes = new List<string>();
+
+            for (int i = 0; i < 9; i++)
+            {
+                nomsComplet.Add("NOM " + "Prénom " + i.ToString());
+                noms.Add("Prénom " + i.ToString());
+                prenoms.Add("Prénom " + i.ToString());
+                telephones.Add("xx-xx-xx-xx-xx " + i.ToString());
+                emails.Add("xxxx@xxx.fr " + i.ToString());
+                adresses.Add("xxxxxxxxxxxxxxx " + i.ToString());
+                codepostal.Add("xxxxx " + i.ToString());
+                villes.Add("xxxx " + i.ToString());
+            }
+
+            ListeClients.ItemsSource = nomsComplet;
         }
         #endregion
 
@@ -49,6 +76,22 @@ namespace Madera_MMB.View_Crtl
         private void BtnRetourListeProjet_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void ListeClients_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListItem item = sender as ListItem;
+
+            if (e.AddedItems.Count > 0)
+            {
+                ClientNom.Text = e.AddedItems[0].ToString();
+                ClientPrenom.Text = e.AddedItems[0].ToString();
+                ClientTelephone.Text = e.AddedItems[0].ToString();
+                ClientEmail.Text = e.AddedItems[0].ToString();
+                ClientAdresse.Text = e.AddedItems[0].ToString();
+                ClientCodePostal.Text = e.AddedItems[0].ToString();
+                ClientVille.Text = e.AddedItems[0].ToString();
+            }
         }
         #endregion
     }
