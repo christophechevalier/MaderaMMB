1c830e15e714e8bfdf0bc25937364cbb27fc1cb5
David <david.drocco@viacesi.fr>
2016-12-14 11:59:58 +0100

    ajout d'images + back code

--- app/Madera MMB/View_Crtl/Modelisation.xaml.cs
+++ app/Madera MMB/View_Crtl/Modelisation.xaml.cs
@@ -23,6 +23,13 @@ namespace Madera_MMB.View_Crtl
     {
 
         private Grid grid = new Grid();
+        private Image croix = new Image();
+        private Image murh = new Image();
+        private Image murv = new Image();
+        private Image anglebd = new Image();
+        private Image anglehd = new Image();
+        private Image anglebg = new Image();
+        private Image anglehg = new Image();
 
         public Modelisation()
         {
@@ -35,9 +42,6 @@ namespace Madera_MMB.View_Crtl
         {
             grid.ShowGridLines = true;
             grid.Margin = new Thickness(7);
-
-
-
 
             ColumnDefinition col;
             RowDefinition row;
@@ -64,38 +68,10 @@ namespace Madera_MMB.View_Crtl
 
         private void initializeImage()
         {
-
-            List<Uri> imagesUri = new List<Uri>();
-
-            Uri Uricroix = new Uri("../Lib/Images/Croix.png", UriKind.Relative);
-            Uri Urimurh = new Uri("../Lib/Images/mur_horizontal.png", UriKind.Relative);
-            Uri Urimurv = new Uri("../Lib/Images/mur_vertical.png", UriKind.Relative);
-            Uri Urianglebd = new Uri("../Lib/Images/angle_bd.png", UriKind.Relative);
-            Uri Urianglehd = new Uri("../Lib/Images/angle_bg.png", UriKind.Relative);
-            Uri Urianglebg = new Uri("../Lib/Images/angle_hd.png", UriKind.Relative);
-            Uri Urianglehg = new Uri("../Lib/Images/angle_hg.png", UriKind.Relative);
-
-
-            imagesUri.Add(Uricroix);
-            imagesUri.Add(Urimurh);
-            imagesUri.Add(Urimurv);
-            imagesUri.Add(Urianglebd);
-            imagesUri.Add(Urianglehd);
-            imagesUri.Add(Urianglebg);
-            imagesUri.Add(Urianglehg);
-
-
-            List<BitmapImage> bitmaps = new List<BitmapImage>();
-
-
-            foreach(Uri uri in imagesUri)
-            {
-                System
-            }
-
             Uri imageUri;
             Thickness margin;
 
+            imageUri = new Uri("../Lib/Images/Croix.png", UriKind.Relative);
             croix.Source = new BitmapImage(imageUri);
             croix.MinWidth = 50;
             croix.MinHeight = 50;
@@ -108,16 +84,92 @@ namespace Madera_MMB.View_Crtl
             margin.Top = -3;
             croix.Margin = margin;
 
+            imageUri = new Uri("../Lib/Images/mur_horizontal.png", UriKind.Relative);
+            murh.Source = new BitmapImage(imageUri);
+            murh.MinWidth = 50;
+            murh.MinHeight = 50;
+            murh.HorizontalAlignment = HorizontalAlignment.Center;
+            murh.VerticalAlignment = VerticalAlignment.Center;
+            margin = murh.Margin;
+            margin.Left = -3;
+            margin.Right = -3;
+            margin.Bottom = -3;
+            margin.Top = -3;
+            murh.Margin = margin;
+
+            imageUri = new Uri("../Lib/Images/mur_vertical.png", UriKind.Relative);
+            murv.Source = new BitmapImage(imageUri);
+            murv.MinWidth = 50;
+            murv.MinHeight = 50;
+            murv.HorizontalAlignment = HorizontalAlignment.Center;
+            murv.VerticalAlignment = VerticalAlignment.Center;
+            margin = murv.Margin;
+            margin.Left = -3;
+            margin.Right = -3;
+            margin.Bottom = -3;
+            margin.Top = -3;
+            murv.Margin = margin;
+
+            imageUri = new Uri("../Lib/Images/angle_bd.png", UriKind.Relative);
+            anglebd.Source = new BitmapImage(imageUri);
+            anglebd.MinWidth = 50;
+            anglebd.MinHeight = 50;
+            anglebd.HorizontalAlignment = HorizontalAlignment.Center;
+            anglebd.VerticalAlignment = VerticalAlignment.Center;
+            margin = anglebd.Margin;
+            margin.Left = -3;
+            margin.Right = -3;
+            margin.Bottom = -3;
+            margin.Top = -3;
+            anglebd.Margin = margin;
+
+            imageUri = new Uri("../Lib/Images/angle_bg.png", UriKind.Relative);
+            anglebg.Source = new BitmapImage(imageUri);
+            anglebg.MinWidth = 50;
+            anglebg.MinHeight = 50;
+            anglebg.HorizontalAlignment = HorizontalAlignment.Center;
+            anglebg.VerticalAlignment = VerticalAlignment.Center;
+            margin = anglebg.Margin;
+            margin.Left = -3;
+            margin.Right = -3;
+            margin.Bottom = -3;
+            margin.Top = -3;
+            anglebg.Margin = margin;
+
+            imageUri = new Uri("../Lib/Images/angle_hd.png", UriKind.Relative);
+            anglehd.Source = new BitmapImage(imageUri);
+            anglehd.MinWidth = 50;
+            anglehd.MinHeight = 50;
+            anglehd.HorizontalAlignment = HorizontalAlignment.Center;
+            anglehd.VerticalAlignment = VerticalAlignment.Center;
+            margin = anglehd.Margin;
+            margin.Left = -3;
+            margin.Right = -3;
+            margin.Bottom = -3;
+            margin.Top = -3;
+            anglehd.Margin = margin;
+
+            imageUri = new Uri("../Lib/Images/angle_hg.png", UriKind.Relative);
+            anglehg.Source = new BitmapImage(imageUri);
+            anglehg.MinWidth = 50;
+            anglehg.MinHeight = 50;
+            anglehg.HorizontalAlignment = HorizontalAlignment.Center;
+            anglehg.VerticalAlignment = VerticalAlignment.Center;
+            margin = anglehg.Margin;
+            margin.Left = -3;
+            margin.Right = -3;
+            margin.Bottom = -3;
+            margin.Top = -3;
+            anglehg.Margin = margin;
         }
 
         private void loadGrid()
         {
-            Button[,] btns = new Button[30,40];
-
+            Button[,] btns = new Button[30, 40];
 
             for (int i = 0; i < 30; i++)
             {
-                for(int y = 0; y < 40; y++) 
+                for (int y = 0; y < 40; y++)
                 {
                     Button but = new Button();
                     Grid.SetRow(but, i);
@@ -127,13 +179,15 @@ namespace Madera_MMB.View_Crtl
                     btns[i, y] = but;
                     grid.Children.Add(but);
 
-                   // Console.WriteLine("---------------------------------- " + but.Name + " ----------------------------------");
+                    //Console.WriteLine("---------------------------------- " + but.Name + " ----------------------------------");
                 }
             }
 
+            btns[2, 2].Content = anglehg;
+            btns[4, 4].Content = anglehg;
 
-            //btns[2, 3].Content = murh;
-            //btns[2, 4].Content = murh;
+            btns[2, 3].Content = murh;
+            btns[2, 4].Content = murh;
 
             btns[3, 2].Content = murv;
             btns[4, 2].Content = murv;
