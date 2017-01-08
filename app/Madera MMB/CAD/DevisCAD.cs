6697cf0ee064be4fe7b18cc07e91c2b3dd63fbdf
GonyLinks <gabriel.bolzinger@viacesi.fr>
2017-01-08 12:41:05 +0100

    Ajout du CAD et Model Devis: - Ajout / Modification de proprietés dans le model - Ajout de la methode listAllDevisParPlan() - Ajout de la methode insertDevis()

--- app/Madera MMB/CAD/DevisCAD.cs
+++ app/Madera MMB/CAD/DevisCAD.cs
@@ -4,26 +4,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Madera_MMB.Lib;
+using System.Data.SQLite;
 
 namespace Madera_MMB.CAD
 {
     class DevisCAD
     {
         #region properties
-        private List<Devis> listedevis { get; set; }
-
+        private List<Devis> listeAllDevisParPlan { get; set; }
+        public Connexion conn { get; set; }
+        public Plan plan { get; set; }
+        public Projet projet { get; set; }
+        public string SQLQuery { get; set; }
+        public ClientCAD clientCAD { get; set; }
+        public CommercialCAD commercialCAD { get; set; }
         #endregion
 
         #region Ctor
-
+        public DevisCAD(Connexion laConnexion, Projet unprojet, ClientCAD clientCAD, CommercialCAD commercialCAD)
+        {
+            this.projet = unprojet;
+            Connexion conn = laConnexion;
+            listeAllDevisParPlan = new List<Devis>();
+            this.clientCAD = clientCAD;
+            this.commercialCAD = commercialCAD;
+        }
         #endregion
 
         #region privates methods
-        private void listAllDevis() { }
-        private void selectDevis(Devis devis) { }
-        private void insertDevis(Devis devis) { }
-        private void updateDevis(Devis devis) { }
+        //REQUETE SELECT DEVIS PAR PLAN
+        private void listAllDevisParPlan() 
+        {
+            SQLQuery = "SELECT * FROM Devis WHERE refPlan = " + plan.reference;
+            SQLiteCommand command = (SQLiteCommand)conn.LiteCo.CreateCommand();
+            command.CommandText = SQLQuery;
+            SQLiteDataReader reader = command.ExecuteReader();
 
+            try
+            {
+                while (reader.Read())
+                {
+                    Devis devis = new Devis();
+                    devis.reference = reader.GetString(0);
+                    devis.nom = reader.GetString(1);
+                    devis.etat = reader.GetString(2);
+                    devis.quantite = reader.GetString(3);
+                    devis.unite = reader.GetString(4);
+                    devis.creation = reader.GetDateTime(5);
+                    devis.margeCommercial = reader.GetInt32(6);
+                    devis.margeEntreprise = reader.GetInt32(7);
+                    devis.prixTotalHT = reader.GetInt32(6);
+                    devis.prixTotalTTC = reader.GetInt32(6);
+                    devis.plan = this.plan;
+                    devis.projet = this.projet;
+
+                    listeAllDevisParPlan.Add(devis);
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+
+        //REQUETE CREATION DEVIS
+        private void insertDevis(Devis devis, string refProjet, string refPlan, string refClient, string refCommercial) 
+        {
+            SQLQuery = "INSERT INTO devis (refDevis, nom, etat, quantite, unite, dateCreation, margeCommercial, margeEntreprise, prixTotalHT, prixTotalTTC, refPlan, refProjet, refClient, refCommercial)" +
+            "VALUES (" + devis.reference + "," + devis.nom + "," + devis.etat + "," + devis.quantite + "," + devis.unite + "," + devis.creation + "," + devis.margeCommercial + "," + devis.margeEntreprise + "," + devis.prixTotalHT + "," + devis.prixTotalTTC + "," + devis.plan.reference + "," + devis.projet.reference + "," + refClient + "," + refCommercial + ";";
+            conn.InsertSQliteQuery(SQLQuery);
+        }
         #endregion
     }
 }
--- app/Madera MMB/Model/Devis.cs
+++ app/Madera MMB/Model/Devis.cs
@@ -6,35 +6,57 @@ using System.Threading.Tasks;
 
 namespace Madera_MMB.Model
 {
+    // TO DO
+    // - properties OK
+    // - ctor OK
+    // - Méthodes privées :
+    //    - calculerPrixHT X
+    //    - calculerPrixTTC X
+    //    - verifierExpiration X
+    //    - changerEtat X
+    //    - verifierOffrePromo X
+    //    - genererDocTech X
+    //
     class Devis
     {
         #region properties
-        public int reference { get; set; }
+        public string reference { get; set; }
         public string nom { get; set; }
         public string etat { get; set; }
-        public int quantite { get; set; }
+        public string quantite { get; set; }
         public string unite { get; set; }
         public DateTime creation { get; set; }
         public int margeCommercial { get; set; }
         public int margeEntreprise { get;set;}
         public int prixTotalHT { get; set; }
         public int prixTotalTTC { get; set; }
+        public Plan plan { get; set; }
+        public Projet projet { get; set; }
+        public Client client { get; set; }
+        public Commercial commercial { get; set; }
+
         #endregion
 
         #region Ctor
-
+        public Devis(Plan unplan, Projet unprojet, Client unclient, Commercial uncommercial)
+        {
+            this.plan = unplan;
+            this.projet = unprojet;
+            this.client = unclient;
+            this.commercial = uncommercial;
+        }
+        public Devis() { }
         #endregion
 
-        //#region privates methods
-        //private void calculerPrixHT();
-        //private void calculerPrixTTC();
-        //private void verifierExpiration();
-        //private void valider();
-        //private void refuser();
-        //private void facturer();
-        //private void verifierOffrePromo();
-        //private void genererDocTech();
+        #region privates methods
 
-        //#endregion
+        private void calculerPrixHT();
+        private void calculerPrixTTC();
+        private void verifierExpiration();
+        private void changerEtat();
+        private void verifierOffrePromo();
+        private void genererDocTech();
+
+        #endregion
     }
 }
